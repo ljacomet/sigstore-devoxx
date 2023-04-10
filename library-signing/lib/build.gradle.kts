9eb57312a3799e973da17479c56600da6290f1a1
Louis Jacomet <ljacomet@gmail.com>
2023-04-10 22:40:56 +0200

    Add publishing and sigstore signing

--- library-signing/lib/build.gradle.kts
+++ library-signing/lib/build.gradle.kts
@@ -1,7 +1,12 @@
 
 plugins {
     `java-library`
+    `maven-publish`
+    id("dev.sigstore.sign") version "0.4.1"
 }
+
+group = "org.example"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
@@ -9,4 +14,20 @@ repositories {
 
 dependencies {
     implementation("com.google.guava:guava:31.1-jre")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven(url = uri("${rootProject.buildDir}/repo"))
+    }
+}
+
+sigstoreSign {
+    sigstoreJavaVersion.set("0.4.0")
 }
